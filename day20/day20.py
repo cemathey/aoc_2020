4d6829497c607ed399e781b8520c878c6ee0f54c
Eric Mathey <emathey@localhost.localdomain>
2020-12-22 22:55:24 -0700

    Working day 20 part 1 and 2, requires cleanup.

--- day20/day20.py
+++ day20/day20.py
@@ -39,48 +39,65 @@ def build_tiles(file_name):
     return tiles
 
 
-def vertical_flip(tile):
-    """Perform a vertical flip (bottom to top) of the given tile and return the new, flipped tile."""
-    new_pixels = tuple(row for row in reversed(tile.pixels))
+def vertical_flip_pixels(pixels):
+    return tuple(row for row in reversed(pixels))
 
-    return Tile(tile.tile_id, tile.size, new_pixels)
+
+def vertical_flip_tile(tile):
+    """Perform a vertical flip (bottom to top) of the given tile and return the new, flipped tile."""
+    return Tile(tile.tile_id, tile.size, vertical_flip_pixels(tile.pixels))
+
+
+def vertical_flip_image(image):
+    """Perform a vertical flip (bottom to top) of the given image and return the new, flipped image."""
+    return Image(image.size, vertical_flip_pixels(image.pixels))
+
+
+def rotate_pixels_90(pixels):
+    size = len(pixels)
+    new_pixels = [[None] * size for _ in range(size)]
+
+    points = deque()
+    col = 0
+    for _ in range(size):
+        for row in range(size - 1, -1, -1):
+            points.append((row, col))
+        col += 1
+
+    for i in range(size):
+        for j in range(size):
+            rot_i, rot_j = points.popleft()
+            new_pixels[i][j] = pixels[rot_i][rot_j]
+
+    return tuple(tuple(row) for row in new_pixels)
+
+
+def rotate_image_90(image):
+    """Perform a 90* clockwise rotation of the given image and return the new, rotated image."""
+    return Image(image.size, rotate_pixels_90(image.pixels))
 
 
 def rotate_tile_90(tile):
     """Perform a 90* clockwise rotation of the given tile and return the new, rotated tile."""
-
-    new_pixels = [[None] * tile.size for _ in range(tile.size)]
-
-    points = deque()
-    col = 0
-    for _ in range(tile.size):
-        for row in range(tile.size - 1, -1, -1):
-            points.append((row, col))
-        col += 1
-
-    for i in range(tile.size):
-        for j in range(tile.size):
-            rot_i, rot_j = points.popleft()
-            new_pixels[i][j] = tile.pixels[rot_i][rot_j]
-
-    new_pixels = tuple(tuple(row) for row in new_pixels)
-
-    return Tile(tile.tile_id, tile.size, new_pixels)
+    return Tile(tile.tile_id, tile.size, rotate_pixels_90(tile.pixels))
 
 
-def rotate_tile(tile, degrees=90):
-    """Rotate the given tile the indicated degrees and return the new, rotated title"""
+def rotate(item, degrees=90):
+    """Rotate the given image or tile the indicated degrees and return the new, rotated item."""
     assert degrees in (90, 180, 270, 360)
 
     times = degrees // 90
 
     if degrees == 360:
-        return tile
+        return item
 
     for _ in range(times):
-        tile = rotate_tile_90(tile)
+        if type(item) == Tile:
+            item = rotate_tile_90(item)
+        elif type(item) == Image:
+            item = rotate_image_90(item)
 
-    return tile
+    return item
 
 
 def top_edge(tile: Tile) -> Tuple[str, ...]:
@@ -103,17 +120,30 @@ def right_edge(tile: Tile) -> Tuple[str, ...]:
     return tuple(row[-1] for row in tile.pixels)
 
 
+def all_image_orientations(image: Image) -> Generator:
+    """Yield all orientations of the given image."""
+    yield image
+    yield vertical_flip_image(image)
+
+    for degree in (90, 180, 270):
+        rotated_image = rotate(image, degree)
+        yield rotated_image
+
+        v_flipped_image = vertical_flip_image(rotated_image)
+        yield v_flipped_image
+
+
 def all_tile_orientations(tile: Tile) -> Generator:
     """Yield all orientations of the given tile."""
     # A horizontal and vertical flip == 180* rotation
     yield tile
-    yield vertical_flip(tile)
+    yield vertical_flip_tile(tile)
 
     for degree in (90, 180, 270):
-        rotated_tile = rotate_tile(tile, degree)
+        rotated_tile = rotate(tile, degree)
         yield rotated_tile
 
-        v_flipped_tile = vertical_flip(rotated_tile)
+        v_flipped_tile = vertical_flip_tile(rotated_tile)
         yield v_flipped_tile
 
 
@@ -205,82 +235,138 @@ def remove_tile_borders(tile: Tile) -> Tile:
 
 
 def remove_borders(grid: Tuple[Tile, ...]):
-    borders_removed = []
+    """Remove the borders from each tile in the grid and return a new grid."""
+    trimmed_grid = []
     for row in grid:
         new_row = []
         for tile in row:
             new_row.append(remove_tile_borders(tile))
-        borders_removed.append(new_row)
+        trimmed_grid.append(new_row)
 
-    return borders_removed
+    return trimmed_grid
 
 
 def grab_corner_tiles(grid: Tuple[Tile, ...]):
+    """Return the corner tiles clockwise from the top left."""
     return (grid[0][0], grid[0][-1], grid[-1][-1], grid[-1][0])
 
 
 def stitch_tiles(grid: Tuple[Tile, ...]) -> Image:
-    """Take the given grid of tiles, remove the border of each tile, and assemble them into one large tile."""
+    """Take the given grid of tiles, remove the border of each tile, and assemble them into one image."""
     trimmed_grid = remove_borders(grid)
-    tile_size = trimmed_grid[0][0].size
     image_size = len(trimmed_grid[0][0].pixels) * len(trimmed_grid[0])
-    pixels = [[None] * image_size for _ in range(image_size)]
 
-    i_offsets = cycle(range(8))
-    j_offsets = cycle(range(1, 24))
+    image_pixels = []
+    for grid_row in trimmed_grid:
+        first_tile = grid_row[0]
+        # Stitch together a complete row from each tile in that row
+        for i, pixel_row in enumerate(first_tile.pixels):
+            image_row = []
+            image_row.extend(pixel_row)
+            for remaining_tile in grid_row[1:]:
+                image_row.extend(remaining_tile.pixels[i])
+            image_pixels.append(image_row)
 
-    # Row 1
-    # tile 1:
-    # i=0 to i=7
-    # j=0 to j=7
+    return Image(image_size, tuple(tuple(row) for row in image_pixels))
 
-    # tile 1:
-    # i=0 to i=7
-    # j=8 to j=15
 
-    # tile 3:
-    # i=0 to i=7
-    # j=16 to j=23
+def fill_grid(tiles_lookup):
+    """Return a solved grid if possible from the given tiles."""
+    for tile in tiles_lookup.values():
+        grid = top_left_corner_possibilities(tiles_lookup, tile)
+        if grid:
+            yield grid
 
-    # Row 2
-    # tile 4:
-    # i=8 to i=15
-    # j=0 to j=7
 
-    # tile 5:
-    # i=8 to i=15
-    # j=8 to j=15
+def count_monsters(image: Image, monster_pixel="#") -> int:
+    monster_point_offsets = [
+        (0, 18),
+        (1, 0),
+        (1, 5),
+        (1, 6),
+        (1, 11),
+        (1, 12),
+        (1, 17),
+        (1, 18),
+        (1, 19),
+        (2, 1),
+        (2, 4),
+        (2, 7),
+        (2, 10),
+        (2, 13),
+        (2, 16),
+    ]
 
-    # tile 6:
-    # i=8 to i=15
-    # j=16 to j=23
+    monster_length = 3
+    monster_width = 21
 
-    i_offsets = [range(0, 8), range(8, 16), range(16, 24)]
-    j_offsets = [range(0, 8), range(8, 16), range(16, 24)]
+    # Iterate through our image and look for monsters
+    monster_count = 0
+    for i, row in enumerate(image.pixels[:-monster_length]):
+        for j, pixel in enumerate(row[:-monster_width]):
+            offset_pixels = [(x + i, y + j) for x, y in monster_point_offsets]
+            matches = [
+                image.pixels[i + x][j + y] == monster_pixel
+                for x, y in monster_point_offsets
+            ]
+            if all(
+                image.pixels[x + i][y + j] == monster_pixel
+                for x, y in monster_point_offsets
+            ):
+                monster_count += 1
 
-    for row in trimmed_grid:
-        for tile in row:
-            for pixel_i, pixel_row in enumerate(tile.pixels):
-                for pixel_j, pixel in enumerate(pixel_row):
-                    pixels[i][j] = pixel
+    return monster_count
 
-    return Image(image_size, tuple(tuple(row) for row in pixels))
+
+def find_correct_image_orientation(grids):
+    for grid in grids:
+        image = stitch_tiles(grid)
+        for test_image in all_image_orientations(image):
+            count = count_monsters(test_image)
+            if count > 0:
+                return test_image, count
+
+    return None, None
 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
     tiles_lookup = build_tiles(filename)
 
-    grid = None
-    for tile in tiles_lookup.values():
-        grid = top_left_corner_possibilities(tiles_lookup, tile)
-        if grid:
-            break
+    grids = list(fill_grid(tiles_lookup))
+    assert len(grids) == 4
 
-    assert grid
-    corner_tiles = grab_corner_tiles(grid)
+    corner_tiles = grab_corner_tiles(grids[0])
+    assert len(corner_tiles) == 4
+
     answer = reduce(lambda total, tile: total * tile.tile_id, corner_tiles, 1)
-    assert answer == 20899048083289
+    # assert answer == 20899048083289
     print(f"Part 1: Corner Tile Product {answer}")
 
-    # stitch_tiles(grids[0])
+    correct_image, monster_count = find_correct_image_orientation(grids)
+
+    pound_count = 0
+    for row in correct_image.pixels:
+        for pixel in row:
+            if pixel == "#":
+                pound_count += 1
+
+    answer = pound_count - monster_count * 15
+    print(f"Part 2: answer={answer}")
+
+    # for row in image.pixels:
+    #     print("".join(row))
+
+    # for row in image.pixels:
+    #     for pixel in row:
+    #         if pixel == "#":
+    #             print(pixel, end="")
+    #         else:
+    #             print(" ", end="")
+    #     print()
+
+    # print()
+    # print()
+
+    # count = count_monsters(image)
+    # print(f"{count=}")
